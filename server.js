8de09c23f9f03c28f182802db9153516f12a850e
saasbook <saasbook@saasbook.(none)>
2012-04-10 23:15:50 -0500

    init

---
+++ server.js
@@ -0,0 +1,8 @@
+
+var http = require("http")
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("guepaje!");
+    response.end();
+}).listen(8888);
