38bba221d8abe17ec855fd5e2ce4c62c8e9d00a8
saasbook <saasbook@saasbook.(none)>
2012-04-13 21:19:50 -0500

    updated for cliente/server chat

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	fs = require('fs'),
+  port = process.env.PORT || 3000;
+	counter = 0;
 
-var http = require("http")
-var heroku_port = process.env.PORT || 3000;
+app.listen(port);
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("guepaje!");
-    response.end();
-}).listen(heroku_port);
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
+io.sockets.on('connection', function (socket) {
+	
+	socket.on('putMessage', function (data) {
+		console.log("the message was: " + data.message);
+		socket.emit('test', { details: data.nick + ': ' + data.message } );
+		socket.broadcast.emit('test', { details: data.nick + ': ' + data.message } );
+	});
+	
+});
