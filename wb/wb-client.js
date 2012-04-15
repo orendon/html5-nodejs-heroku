caf134f0d224e4930ffb97a47df7de805ad6cb34
Oscar Rendon <oarendon@gmail.com>
2012-04-14 20:11:15 -0500

    implemented drawing on mouse move

--- wb/wb-client.js
+++ wb/wb-client.js
@@ -1,6 +1,45 @@
 
-document.onload = start;
+var canvas,
+    ctx,
+    mousePressed = false,
+    x = 0,
+    y = 0;
+
+window.onload = start;
 
 function start() {
-  var ctx = document.getElementById('wbCanvas').getContext('2d');
+  canvas = document.getElementById("wbCanvas");
+  ctx = canvas.getContext("2d");
+  console.log("started");
+  canvas.addEventListener("mousemove", onMouseMove, false);
 }
+
+function onMousePress(e) {
+  //ctx.beginPath();
+  //ctx.moveTo(x, y);
+  mousePressed = true;
+}
+
+function onMouseMove(e) {
+  if (e.layerX || e.layerX == 0) { // Firefox
+    x = e.layerX;
+    y = e.layerY;
+  }
+  else if (e.offsetX || e.offsetX == 0) { // Opera
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+  if(mousePressed) {
+    drawLine(x, y);
+  }
+  else {
+    onMousePress();
+  }
+  console.log(x + "," + y);
+}
+
+function drawLine(x, y) {
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
