64d07ec8d1aca02aa23db6de16a7851ac7b02ef5
Oscar Rendon <oarendon@gmail.com>
2012-04-14 20:27:08 -0500

    clean drawing path on mousepress release

--- wb/wb-client.js
+++ wb/wb-client.js
@@ -13,18 +13,25 @@ function start() {
 
   canvas.addEventListener("mousemove", onMouseMove, false);
   canvas.addEventListener("mousedown", onMousePress, false);
+  canvas.addEventListener("mouseup", onMousePress, false);
 }
 
 function onMousePress(e) {
-  mousePressed = true;
+  if(mousePressed) {
+    mousePressed = false;
+  }
+  else {
+    ctx.moveTo(x, y);
+    mousePressed = true;
+  }
 }
 
 function onMouseMove(e) {
-  if (e.layerX || e.layerX == 0) { // Firefox
+  if (e.layerX || e.layerX == 0) { // firefox
     x = e.layerX;
     y = e.layerY;
   }
-  else if (e.offsetX || e.offsetX == 0) { // Opera
+  else if (e.offsetX || e.offsetX == 0) { // opera
     x = e.offsetX;
     y = e.offsetY;
   }
