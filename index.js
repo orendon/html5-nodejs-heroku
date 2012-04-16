207b3e503974f95b9fbd818f04560b66dd2852d4
Oscar Rendon <oarendon@gmail.com>
2012-04-15 19:15:17 -0500

    decouple socket events from sockets.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var server = require('./server.js'),
     handler = require('./handlers'),
     sockets = require('./sockets.js');
 
-var socket = new sockets.SocketServer;
+var events = {
+  chat: './chat/chat-socket.js'
+};
+var socket = new sockets.SocketServer(events);
 
 var handlers = {};
 handlers['/'] = handler.index;
--- sockets.js
+++ sockets.js
@@ -1,26 +1,24 @@
 
-function SocketServer() {
+function SocketServer(events) {
   this.socketio = require('socket.io');
-  
+
   var socketio = this.socketio;
   var io;
 
   this.listen= function(app) {
     io = socketio.listen(app);
-    
+
     //heroku configuration
     io.configure(function () {
       io.set("transports", ["xhr-polling"]);
       io.set("polling duration", 10);
     });
 
-    //socket handlers
+    //bind socket events
     io.sockets.on('connection', function (socket) {
-        socket.on('putMessage', function (data) {
-          console.log("the message was: " + data.message);
-          socket.emit('test', { details: data.nick + ': ' + data.message } );
-          socket.broadcast.emit('test', { details: data.nick + ': ' + data.message } );
-        }); 
+        for(event in events) {
+          require(events[event]).bind(socket);
+        }
     });
   }
 
